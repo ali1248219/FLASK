97d871c54c92da7daa740c4008045cab40e6c303
ali1248219 <alirelevan@gmail.com>
2023-05-25 21:16:58 +0700

    tutorial finished

--- database/sql.sql
+++ database/sql.sql
@@ -1,6 +1,6 @@
 -- CREATE TABLE USERS(username TEXT NOT NULL PRIMARY KEY,password NOT NULL,email NOT NULL);
 
--- INSERT INTO USERS(username,password,email) VALUES ("johnson", "notmypassword", "email@gmail");
+-- INSERT INTO USERS(username,password,email) VALUES ("fead", "sadasd", "email23@gmail");
 
 -- SELECT * FROM USERS;
 
@@ -13,3 +13,11 @@
 -- DELETE FROM USERS;
 
 -- DROP TABLE IF EXISTS USERS;
+
+-- CREATE TABLE USERS(id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT NOT NULL, satker TEXT NOT NULL, bidang TEXT NOT NULL);
+
+-- INSERT INTO USERS(name, satker, bidang) VALUES ('Aul' , 'Biro Pengadaan' , 'PBJ');
+
+-- SELECT * FROM USERS;
+
+-- UPDATE USERS SET id='2' WHERE id ='4';
